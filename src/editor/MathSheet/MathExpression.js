02466527e7e0dfcffe63a1b8782afa58a98c869e
alexandreLamarre <alex7285@gmail.com>
2020-12-15 12:30:06 -0500

    math expression and codeblock templates

---
+++ src/editor/MathSheet/MathExpression.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./MathExpression.css";
+
+class MathExpression extends React.Component{
+  constructor(props){
+      super(props);
+      this.state = {
+
+      }
+  }
+
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
